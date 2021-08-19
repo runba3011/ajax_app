686589e9c2db81a68080c84472742b90e1e6f2bd
runba3011 <runba3011@gmail.com>
2021-08-19 18:17:07 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,20 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("id DESC")
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
+
+  private 
+  def post_params
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
